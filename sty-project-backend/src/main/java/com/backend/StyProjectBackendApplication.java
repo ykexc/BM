c509417b6535c0a7fa3d23f9507eae5a1d5abaf4
rtxsrp <3479103351@qq.com>
2023-08-07 22:21:41 +0800

    feat: init

---
+++ sty-project-backend/src/main/java/com/backend/StyProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StyProjectBackendApplication.class, args);
+    }
+
+}
